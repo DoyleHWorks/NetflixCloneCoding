fe8608b6899009e9048f58a1527ff69e8a4b2863
DoyleHWorks <DoyleHWorks@gmail.com>
2024-12-24 20:59:14 +0900

    🎉 [Init] Project Initialization
    Added Libraries:
    Alamofire, RxSwift, SnapKit, Then, Youtube-iOS-Player-Helper
    [Update] .gitignore: .DS_Store

---
+++ NetflixClone/NetflixClone/Sources/Views/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  NetflixClone
+//
+//  Created by t0000-m0112 on 2024-12-24.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
